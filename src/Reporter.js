ecb77ab84c5ab6c00dd5b01ee458fbe9f5c53b34
Jeroen Visser <jeroenvisser101@gmail.com>
2015-01-13 22:12:42 +0100

    Binding to prototyped object.

---
+++ src/Reporter.js
@@ -0,0 +1,7 @@
+// Make sure Clepta is defined
+window.Clepta = window.Clepta || {};
+
+/**
+ * The role of the reporter is to report data to a control server.
+ */
+Clepta.Reporter = function () {};
