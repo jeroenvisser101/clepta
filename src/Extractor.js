b6e74178f4fdae48f45607f65abff58dde890ab2
Jeroen Visser <jeroenvisser101@gmail.com>
2015-01-14 19:16:03 +0100

    Added extractor, created PoC HTML file.

---
+++ src/Extractor.js
@@ -0,0 +1,53 @@
+// Make sure Clepta is defined
+window.Clepta = window.Clepta || {};
+
+/**
+ * Extractor
+ * 
+ * The extractor's job is to extract usernames and passwords from forms.
+ */
+Clepta.Extractor = function (forms) {
+  this.forms = forms;
+
+  /**
+   * Retrieve a label for a given input from the given context.
+   *
+   * @return {String|null}
+   */
+  var getLabelForInput = function (context, input) {
+    // If there is no id for this input, there is no label.
+    if (typeof input.id === 'undefined') return null;
+
+    // Get the label for the given input
+    var label = context.querySelector('label[for="' + input.id + '"');
+    if (typeof label === 'undefined') return null; // Return null if none found
+
+    return label ? label.textContent || null : null;
+  };
+
+  /**
+   * Extracts the date from the form in the current context.
+   *
+   * @return {}
+   */
+  this.extract = function () {
+    return _.map(this.forms, function (form) {
+      // Get all form inputs from the context's form
+      var formInputs = form.querySelectorAll('input'),
+          data = [];
+
+      _.each(formInputs, function (input) {
+        data.push({
+          id: input.id || null,
+          name: input.name || null,
+          type: input.type || 'text',
+          label: getLabelForInput(form, input) || null,
+          placeholder: input.placeholder || null,
+          value: input.value || null
+        });
+      });
+
+      return data;
+    });
+  };
+};
--- src/Finder.js
+++ src/Finder.js
@@ -12,40 +12,35 @@ window.Clepta = window.Clepta || {};
  */
 Clepta.Finder = function (document) {
   this.document = document || window.document;
-};
 
-/**
- * Ask Clepta to find all login forms.
- *
- * Clepta's login finder will find forms based on the form's field types.
- * For instance, a login box would (most likely) contain both a normal field,
- * for either username or email, and a field for a secret, most likely a
- * password.
- *
- * @return {DOMElement[]} loginForms The forms found to be a login form.
- */
-Clepta.Finder.findLoginForms = function () {
-  var loginForms;
+  /**
+   * Ask Clepta to find all login forms.
+   *
+   * Clepta's login finder will find forms based on the form's field types.
+   * For instance, a login box would (most likely) contain both a normal field,
+   * for either username or email, and a field for a secret, most likely a
+   * password.
+   *
+   * @return {DOMElement[]} loginForms The forms found to be a login form.
+   */
+  this.findLoginForms = function () {
+    var loginForms = [];
 
-  // Get all forms in the document
-  var forms = this.document.querySelectorAll('form');
+    // Get all forms in the document
+    var forms = this.document.querySelectorAll('form');
 
-  // Loop through all forms, keep if the form contains both a password and a
-  // normal text field
-  for (var key in forms) {
-    if (!this.formFields.hasOwnProperty(key)) continue;
+    // Loop through all forms, keep if the form contains both a password and a
+    // normal text field
+    _.each(forms, function (form) {
+      // Check if the form contains both a password field and normal text field
+      if (form.querySelector('input[type="password"]') &&
+          form.querySelector('input[type="text"], input:not([type])')) {
+        // Add this form to the list of login forms
+        loginForms.push(form);
+      }
+    });
 
-    // Retrieve value from forms array
-    var form = forms[key];
-
-    // Check if the form contains both a password field and normal text field
-    if (form.querySelector('input[type="password"]') &&
-        form.querySelector('input[type="text"], input:not([type])')) {
-      // Add this form to the list of login forms
-      loginForms.push(form);
-    }
-  }
-
-  // Return all forms found
-  return loginForms;
+    // Return all forms found
+    return loginForms;
+  };
 };
